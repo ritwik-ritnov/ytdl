d75f4925fd3e8588c774f623af255f1332179cdd
sdutta <souvik.dutta@optimizeitsystems.com>
2021-04-03 00:33:54 +0530

    xnmm

--- youtube_dl/extractor/xnnm.py
+++ youtube_dl/extractor/xnnm.py
@@ -53,7 +53,8 @@ class XnnmIE(InfoExtractor, ABC):
         print(res.__len__())
         entries = []
         http_headers = {
-            'Referer': 'https://members.purenudism.com/'
+            'Referer': 'https://members.purenudism.com/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
         }
         for link in res:
             entry = {
